c3255ca127d44d766eda79af295b3ac8b130cc33
confucuis <confucuis@github.com>
2023-11-16 06:59:01 +0000

    ..

---
+++ src/server.h
@@ -0,0 +1,18 @@
+// 服务器接口
+
+#ifndef _SERVER_H_
+#define _SERVER_H_
+
+class TCPServer
+{
+private:
+    int port;
+    const char *host = "0.0.0.0";
+
+public:
+    TCPServer(int port);
+    ~TCPServer(void);
+    init_tcpserver(void);
+}
+
+#endif // _SERVER_H_
