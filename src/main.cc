0e1867e0e5c783c79038bf23f858e9238687b0b0
confucuis <confucuis@github.com>
2023-11-20 11:44:41 +0800

    ..

--- src/main.cc
+++ src/main.cc
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
+#include "server.h"
+
 int main(void)
 {
+    TCPServer tcp_server(8000);
+    tcp_server.init_tcpserver();
     return 0;
 }
--- src/server.cc
+++ src/server.cc
@@ -1,1 +1,18 @@
+#include <stdio.h>
+
 #include "server.h"
+
+TCPServer::TCPServer(int port)
+{
+    this->port = port;
+}
+
+TCPServer::~TCPServer(void)
+{
+
+}
+
+void TCPServer::init_tcpserver(void)
+{
+    printf("%d\n", this->port);
+}
